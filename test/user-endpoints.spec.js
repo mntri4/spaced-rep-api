860d4644c038a955fe1c6723646502caa8adf789
Mario Martinez <mariom@thinkful.com>
2020-02-18 22:08:54 -0600

    should added to user-endpont.spec.js linr 58

--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -55,7 +55,7 @@ describe('User Endpoints', function () {
       return supertest(app)
         .post('/api/user')
         .send(userShortPassword)
-        .expect(400, { error: `Password be longer than 8 characters` })
+        .expect(400, { error: `Password should be longer than 8 characters` })
     })
 
     it(`responds 400 'Password be less than 72 characters' when long password`, () => {
